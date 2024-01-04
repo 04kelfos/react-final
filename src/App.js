f43c00055d45eec6ec2b80bd397fc838c8ebef2f
04kelfos <kelseybetty@hotmail.com>
2024-01-04 13:05:02 +0900

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="weatherApp">
-      <h1> Weather App </h1>
+    <div className="App">
+      <div className="container">
+        <h1> Weather App </h1>
+        <footer>
+          This project was coded by Kelsey Foster and is open-source on{" "}
+          <a href="https://github.com/04kelfos/react-final" target="_blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
